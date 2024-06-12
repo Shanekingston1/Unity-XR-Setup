0df60cbc2cb4b9c10009ee93cf18a1d1e30f9d86
Sudhakar Kumar <shane.kingston@eliteitteam.com>
2024-06-12 19:59:45 +0500

    new commit
    -Added Background Music
    -Changed room texture.
    - Added built in Player controller and removed scripts.

--- Assets/Scripts/AudioScript.cs
+++ Assets/Scripts/AudioScript.cs
@@ -7,27 +7,11 @@ public class AudioScript : MonoBehaviour
 
     private void Awake() 
     {
-        ballBounce = GetComponent<AudioSource>();
-
-          if (ballBounce == null)
-        {
-            Debug.LogError("AudioSource component not found on " + gameObject.name);
-        }
-        else
-        {
-            Debug.Log("AudioSource component found on " + gameObject.name);
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+          ballBounce = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collision detected with " + collision.gameObject.name);
-        
+    {   
         ballBounce.Play();
     }
 }
